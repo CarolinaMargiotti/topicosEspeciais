c38accaed4bfe8f147481137b06c24e072bdd57a
carolinam <carolinamargiott2i@gmail.com>
2022-08-22 09:41:32 -0300

    estrutura completa

---
+++ src/main/java/com/carolinam/topicosespeciais/TopicosespeciaisApplication.java
@@ -0,0 +1,13 @@
+package com.carolinam.topicosespeciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicosespeciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicosespeciaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carolinam/topicosespeciais/TopicosespeciaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolinam.topicosespeciais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicosespeciaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
