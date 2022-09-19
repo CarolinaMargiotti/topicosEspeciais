13a5225063829fedc1dfdf33cde8126c13355d5b
Carolina Margiotti <carolinamargiott2i@gmail.com>
2022-09-19 13:41:21 +0000

    removido import inutil

--- src/main/java/com/carolinam/topicosespeciais/controller/loginController.java
+++ src/main/java/com/carolinam/topicosespeciais/controller/loginController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.carolinam.topicosespeciais.entity.Autorizacao;
 import com.carolinam.topicosespeciais.security.JwtUtils;
 import com.carolinam.topicosespeciais.security.Login;
 
